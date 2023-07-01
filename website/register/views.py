40f2abe08348bc5f22ea9c8bbf16944515c3fd6e
TaleeBarbieri <talee.alexei@hotmail.com>
2023-07-01 15:28:31 +0100

    working home page navbar

---
+++ website/register/views.py
@@ -0,0 +1,25 @@
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
+from .forms import SignUpForm
+
+
+
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            messages.success(request, "Account successfully created!" )
+            return redirect('/account/login')
+
+    else:
+        form = SignUpForm()
+
+    return render(request, "signup.html", {"form":form})
