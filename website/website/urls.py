cfffa0a74df7ab34e54b434f9f897cbe1223daf9
TaleeBarbieri <talee.alexei@hotmail.com>
2023-07-01 17:03:00 +0100

    changes to home page

--- website/website/urls.py
+++ website/website/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-from register import views as v
+from .register import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
